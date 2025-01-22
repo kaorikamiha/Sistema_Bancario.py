c73b6954bd5e5c9a4cef1b224fc60cf3da360e2b
Kaori <51300284+kaorikamiha@users.noreply.github.com>
2025-01-22 11:04:20 -0300

    Add files via upload

--- primeiro_desafio.py
+++ primeiro_desafio.py
@@ -30,34 +30,34 @@ while True:
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
-            saldo += valor
+            saldo = depositar(saldo, valor)
             extrato += f"Depósito: R$ {valor:.2f}\n"
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("O valor informado é inválido.")
 
     elif opcao == "s":
         valor = float(input("Informe o valor do saque: "))
 
         if valor > saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+            print("Você não tem saldo suficiente.")
 
         elif valor > limite:
-            print("Operação falhou! O valor do saque excede o limite.")
+            print("O valor do saque excede o limite.")
 
         elif numero_saques >= LIMITE_SAQUES:
-            print("Operação falhou! Número máximo de saques excedido.")
+            print("Número máximo de saques excedido.")
 
         elif valor > 0:
             numero_saques, saldo = sacar(numero_saques, saldo, valor)
             extrato += f"Saque: R$ {valor:.2f}\n"
             
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("O valor informado é inválido.")
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print("NSem movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
